f05b370060f06cd92fdbea62eb6da6af91a5c0f6
p33t <p33t@localhost>
2024-06-19 23:06:37 -0700

    Add apicontroller 'Controller1'

---
+++ webapi/controller/Controller1.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller1 : ControllerBase
+    {
+    }
+}
