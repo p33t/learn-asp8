e5987c0ed1a39465e808c2d26ce0b7d00b1a3e5f
p33t <p33t@localhost>
2024-06-29 22:07:22 -0700

    Work around for FromQuery object camelCase query args

--- webapi/controller/Controller1.cs
+++ webapi/controller/Controller1.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webapi.controller;
@@ -7,8 +11,27 @@ namespace webapi.controller;
 public class Controller1 : ControllerBase
 {
     [HttpGet("hello")]
-    public Task<ActionResult<string>> Hello()
+    public Task<ActionResult<string>> Hello([FromQuery] HelloParam parameter)
     {
         return Task.FromResult<ActionResult<string>>(Ok("World"));
+    }
+
+    [HttpGet("getObj")]
+    public Task<ActionResult<HelloParam>> GetObj()
+    {
+        return Task.FromResult<ActionResult<HelloParam>>(Ok(new HelloParam
+        {
+            name = "Macbeth"
+        }));
+    }
+    
+    // Seems to be the easiest way to have a FromQuery object with proper camelCase query args.
+    // Response object automatically have camelCase names when property is PascalCase.
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class HelloParam
+    {
+        [Required(AllowEmptyStrings = false)]
+        [DefaultValue("Bruce")]
+        public string name { get; set; } = string.Empty;
     }
 }
