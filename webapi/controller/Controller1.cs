6a36f6995214ffab1c087b759b617bb810286cac
p33t <p33t@localhost>
2025-04-25 16:29:19 -0700

    Illustrate need for implicit <summary> tag

--- webapi/controller/Controller1.cs
+++ webapi/controller/Controller1.cs
@@ -19,6 +19,7 @@ public class Controller1 : ControllerBase
         return Task.FromResult<ActionResult<string>>(Ok("World"));
     }
 
+    /// These comments that don't have "summary" tag do not appear
     [HttpGet("getObj")]
     public Task<ActionResult<HelloParam>> GetObj()
     {
